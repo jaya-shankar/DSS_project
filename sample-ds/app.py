a7c8fcc01930efda31705dfb51366bba7a608190
jaya-shankar <bjayashankar2102@gmail.com>
2022-05-16 01:18:28 +0530

    final combined_df added

--- sample-ds/app.py
+++ sample-ds/app.py
@@ -1,6 +1,8 @@
+covid19_csv_path = './covid19.csv'
+
 def find_top_confirmed(n = 15):
     import pandas as pd
-    corona_df = pd.read_csv('/content/DSS_project/sample-ds/covid19.csv')
+    corona_df = pd.read_csv(covid19_csv_path)
     corona_df=corona_df[corona_df['Country_Region']=='US']
     by_state = corona_df.groupby('Province_State').sum()[['Confirmed', 'Deaths', 'Recovered', 'Active']]
     cdf = by_state.nlargest(n, 'Confirmed')[['Confirmed']]
@@ -9,19 +11,19 @@ cdf=find_top_confirmed()
 pairs=[(Province_State,confirmed) for Province_State,confirmed in zip(cdf.index,cdf['Confirmed'])]
 import folium
 import pandas as pd
-corona_df = pd.read_csv('/content/DSS_project/sample-ds/covid19.csv')
+corona_df = pd.read_csv(covid19_csv_path)
 corona_df=corona_df[corona_df['Country_Region']=='US']
 
-corona_df=corona_df[['Lat','Long_','Confirmed']]
+corona_df=corona_df[['Province_State','Lat','Long_','Confirmed']]
 corona_df=corona_df.dropna()
 m=folium.Map(location=[30.4407159,-66.6017203],
             tiles='Stamen toner',
             zoom_start=3)
 def circle_maker(x):
-    folium.Circle(location=[x[0],x[1]],
-                 radius=float(x[2]),
+    folium.Circle(location=[x[1],x[2]],
+                 radius=float(x[3]),
                  color="red",
-                 popup='confirmed cases:{}'.format(x[2])).add_to(m)
+                 popup=x[0]+'\nconfirmed cases:{}'.format(x[3])).add_to(m)
 corona_df.apply(lambda x:circle_maker(x),axis=1)
 html_map=m._repr_html_()
 from flask import Flask,render_template,request
