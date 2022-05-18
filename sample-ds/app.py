b709aa34238da91894ee8613c864896f1bc1c0d7
jaya-shankar <bjayashankar2102@gmail.com>
2022-05-19 02:49:27 +0530

    changed server code, add new feature

--- sample-ds/app.py
+++ sample-ds/app.py
@@ -1,52 +1,70 @@
 from tensorflow.keras.models import load_model
-
-covid19_csv_path = './covid19.csv'
-general_csv_path = './combined_general_data.csv'
-#load saved model
-loaded_model = load_model('./my_saved_model')
-loaded_model.compile(metrics=['accuracy'])
-
-def find_top_confirmed(n = 15):
-    import pandas as pd
-    corona_df = pd.read_csv(covid19_csv_path)
-    corona_df=corona_df[corona_df['Country_Region']=='US']
-    by_state = corona_df.groupby('Province_State').sum()[['Confirmed', 'Deaths', 'Recovered', 'Active']]
-    cdf = by_state.nlargest(n, 'Confirmed')[['Confirmed']]
-    return cdf
-cdf=find_top_confirmed()
-pairs=[(Province_State,confirmed) for Province_State,confirmed in zip(cdf.index,cdf['Confirmed'])]
 import folium
 import pandas as pd
-corona_df = pd.read_csv(covid19_csv_path)
-corona_df=corona_df[corona_df['Country_Region']=='US']
-
-corona_df=corona_df[['Province_State','Lat','Long_','Confirmed']]
-corona_df=corona_df.dropna()
-m=folium.Map(location=[30.4407159,-66.6017203],
-            tiles='Stamen toner',
-            zoom_start=3)
-def circle_maker(x):
-    folium.Circle(location=[x[1],x[2]],
-                 radius=float(x[3]),
-                 color="red",
-                 popup=x[0]+'\nconfirmed cases:{}'.format(x[3])).add_to(m)
-corona_df.apply(lambda x:circle_maker(x),axis=1)
-html_map=m._repr_html_()
 from flask import Flask,render_template,request
 from pymongo import MongoClient
+from state_codes import state_code_name_map
+
+general_csv_path    = './combined_general_data.csv'
+final_combined_path = './final_combined_df.csv'
+
+#load saved model
+# loaded_model = load_model('./my_saved_model')
+# loaded_model.compile(metrics=['accuracy'])
+
+def find_confirmed_cases(date):
+    df = pd.read_csv(final_combined_path)
+    df = df[df.date == date]
+    df = df.sort_values(by=['cases'], ascending=False)
+    return df
+def find_top_confirmed(df,n = 15):
+    cdf = df.head(n)
+    return cdf
+
+def generate_state_code_cases_pairs(df):
+    pairs = []
+    df = df.head(15)
+    for index, row in df.iterrows():
+        pairs.append((get_state_name(row['state_code']),row['cases']))
+    return pairs
+
+def circle_maker(x,m):
+    folium.Circle(location=[x.lat,x.long],
+                 radius=float(x.cases)*1.5,
+                 color="red",
+                 fill=True,
+                 weight=1,
+                 popup=get_state_name(x.state_code)+'\nconfirmed cases:{}'.format(x.cases)).add_to(m)
+    
+
 
 client=MongoClient('localhost',27017)
 db=client.covid_db
 todos=db.todos
+
 app=Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         if request.form.get('action1') == 'Estimate risk':
             return render_template("estimate.html")
-        elif  request.form.get('action2') == 'Show statistics':
-            return render_template("statistics.html",table=cdf, cmap=html_map,pairs=pairs)
         else:
+            m=folium.Map(location=[39.8283,-98.5795],
+                tiles='Stamen toner',
+                zoom_start=4,
+                min_zoom = 4,
+                max_zoom = 6)
+            try:
+                date               = request.form.get('date')
+            except:
+                date               = '2021-01-12'
+            # get data frame for that date
+            cases_df            = find_confirmed_cases(date)
+            top_15_states_df    = find_top_confirmed(cases_df,n=15)
+            pairs               = generate_state_code_cases_pairs(cases_df)
+            cases_df.apply(lambda x:circle_maker(x,m=m),axis=1)
+            html_map=m._repr_html_()
+            return render_template("statistics.html",table=top_15_states_df, cmap=html_map,pairs=pairs,date=date)
             pass # unknown
     elif request.method == 'GET':
         return render_template('index.html')
@@ -79,6 +97,11 @@ def predict_risk(data):
     combined_df = pd.concat([general_df,specific_df],axis=1)
     risk_level = loaded_model.predict(combined_df)
     return risk_level
+
+def get_state_name(state_code):
+    return state_code_name_map[state_code]
+    
+
 
 if __name__=="__main__":
     app.run(debug=True)
